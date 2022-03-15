c37b271efd02c180340eb15d800ecdb59a729ec6
ssn00zee <cjavillo@my.bcit.ca>
2022-03-15 04:12:58 -0700

    modal added

--- Mid-Term/comps/info.js
+++ Mid-Term/comps/info.js
@@ -5,21 +5,43 @@ var template_info = document.createElement("template"); //<template> </template>
 template_info.innerHTML = `
 <style>
 .info {
+    display: flex;
+    justify-content: center;
     width: 20em;
     height: 30em;
     background: #2F2885;
     box-shadow: 16px 13px 8px rgba(0, 0, 0, 0.25);
 }
+
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  }
+
 </style>
 
 
-<div class='info'>
+<div class='info' id=>
     <div class='good'></div>
     <div class='bad'></div>
+    </div>
+
+<div id='myModal' class='modal'>
+    <div class='info'></div>
 </div>
-`;
+    `;
 
 //MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
+var modal = this.shadowRoot.querySelector('.modal');
+
 class TheInfo extends HTMLElement {
 
     //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
@@ -33,9 +55,38 @@ class TheInfo extends HTMLElement {
     //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
     connectedCallback(){
         this.shadowRoot.appendChild(template_info.content.cloneNode(true)); //use the template to make a clone
+        // this.shadowRoot.querySelector('.good').onclick = () => this.slideRight();
+        // this.shadowRoot.querySelector('.good').onclick = () => this.slideLeft();
+        document.querySelector('.part1').onclick = () => this.modalOpen();
+        window.onclick = () => this.modalClickAway();
     }
 
-    //To-do - CREATE THE FUNCTIONALITIES HERE!
+    //To-do - CREATE THE FUNCTIONALITIES HERE!}   
+
+    // slideRight(){
+    // this.shadowRoot.querySelector('.info').style.cssText = `
+    //     left:50vw;
+    // `
+    // }
+
+    // slideLeft(){
+    //     this.shadowRoot.querySelector('.info').style.cssText = `
+    //         left:200vw;
+    //     `
+    //     }
+    
+
+
+    modalOpen() {
+        modal.style.display = "block";
+    }
+
+    modalClickAway(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+    }
+
 }
 
 //MUST HAVE - define the tag for the custom elements
--- Mid-Term/comps/particles.js
+++ Mid-Term/comps/particles.js
@@ -39,6 +39,43 @@ particles_comp.innerHTML = `
 }
 
 
+.modal {
+display: none; /* Hidden by default */
+position: fixed; /* Stay in place */
+z-index: 1; /* Sit on top */
+left: 0;
+top: 0;
+width: 100%; /* Full width */
+height: 100%; /* Full height */
+overflow: auto; /* Enable scroll if needed */
+background-color: rgb(0,0,0); /* Fallback color */
+background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+      
+/* Modal Content/Box */
+.modal-content {
+background-color: #2F2885;
+margin: 15% auto; /* 15% from the top and centered */
+padding: 10px;
+border: 1px solid #888;
+height: 30em;
+width: 20em; /* Could be more or less, depending on screen size */
+}
+      
+/* The Close Button */
+.close {
+color: #fafafa;
+float: right;
+font-size: 28px;
+font-weight: bold;
+}
+      
+.close:hover,
+.close:focus {
+color: black;
+text-decoration: none;
+cursor: pointer;
+} 
 
 </style>
 
@@ -47,9 +84,22 @@ particles_comp.innerHTML = `
     <div class='part2'></div>
 </div>
 
+<div id="myModal" class="modal">
+
+    <div class="modal-content">
+
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+
+</div>
+    
 `;
 
 //MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
+
+// var modal = this.shadowRoot.getElementById("myModal");
+// var btn = this.shadowRoot.getElementById("part");
+// var span = this.shadowRoot.getElementsByClassName("close")[0];
 class TheComp extends HTMLElement {
 
     //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
@@ -63,9 +113,44 @@ class TheComp extends HTMLElement {
     //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
     connectedCallback(){
         this.shadowRoot.appendChild(particles_comp.content.cloneNode(true)); //use the template to make a clone
+        // this.shadowRoot.querySelector('.part1').onclick = () => this.slideRight();
+        // window.addEventListener("click", function(event)) = () => this.modalClickAway;
+        this.shadowRoot.querySelector('.part1').onclick = function() {
+            this.shadowRoot.getElementById('myModal').style.display = "block";
+        };
+
+        this.shadowRoot.getElementsByClassName('close')[0].onclick = function() {
+            this.shadowRoot.getElementById('myModal').style.display = "none";
+        };
+
+        window.onclick = function(event) {
+            if (event.target == this.shadowRoot.getElementById('myModal')) {
+              this.shadowRoot.getElementById('myModal').style.display = "none";
+            }
+          } ;
+
+        this.shadowRoot.querySelector('.part1').onclick = () => this.test();
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
+
+    test(){
+        alert('hello');
+    }
+
+    slideRight(){
+        document.querySelector('.info').style.cssText = `
+            display: flex;
+        `
+    }
+
+    modalClickAway(event) {
+        if (event.target == modal) {
+            modal.style.cssText = `
+                display: none;
+            `;
+        }
+    }
 }
 
 //MUST HAVE - define the tag for the custom elements
